b72469312157a2c7b9d7074df21efc2e76d638bb
rakesh479 <48939753+rakesh479@users.noreply.github.com>
2019-06-18 16:45:23 +0530

    Update Webapplication.sh

--- Webapplication.sh
+++ Webapplication.sh
@@ -4,7 +4,4 @@ sudo apt install apache2
 sudo systemctl start apache2
 update-rc.d apache2 defaults
 cd /var/www/html
-echo "
-<html>
-<h1>Welcome to Deloitte.</h1>
-</html>" > index.html
+echo "<html><h1>Welcome to Deloitte.</h1></html>" > index.html
