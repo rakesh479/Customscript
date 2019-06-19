3909f7fec4ecce66339b651a02567c9a8a049438
rakesh479 <48939753+rakesh479@users.noreply.github.com>
2019-06-19 13:09:45 +0530

    Update Webapplication.sh

--- Webapplication.sh
+++ Webapplication.sh
@@ -1,9 +1,8 @@
 sudo su
-chmod +x Webapplication.sh
 #!/bin/bash
 sudo apt update && sudo apt upgrade
 sudo apt install apache2 -y
 sudo systemctl start apache2 
 sudo update-rc.d apache2 defaults
-cd /var/www/html
+cd /var/www/html || exit
 echo "<html><h1>Welcome to Deloitte.</h1></html>" > index.html
